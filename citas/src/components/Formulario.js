c575843db62fbaec5ede05745344fac822837332
kervisvasquez1993 <kervisvasquez24@gmail.com>
2020-09-24 17:57:14 -0400

    agregamos formulario a componente de Formulario.js

--- citas/src/components/Formulario.js
+++ citas/src/components/Formulario.js
@@ -1,7 +1,54 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 const Formulario = () => {
   return(
-      <h1>Hola desde Formulario</h1>
+
+    <Fragment>
+        <h2>Crear Orden</h2>
+        <form>
+            <label>Nombre </label>
+            <input
+                type="text"
+                name="mascota"
+                className="u-full-width"
+                placeholder="Nombre"
+            />   
+
+            <label>Propietario </label>
+            <input
+                type="text"
+                name="propietario"
+                className="u-full-width"
+                placeholder="Propietario"
+            /> 
+            <label>Fecha</label>
+            <input
+                type="date"
+                name="fecha"
+                className="u-full-width"
+                
+            /> 
+            <label>Hora</label>
+            <input
+                type="time"
+                name="hora"
+                className="u-full-width"
+            /> 
+             <label>Sintomas</label>
+            <textarea
+                name="sintomas"
+                className="u-full-width"
+            ></textarea>
+            <button 
+                type="submit"
+                className="u-full-width"
+                > Agregar Orden</button>
+            
+
+              
+              
+        </form>
+    </Fragment>
+      
   )
 }
 
