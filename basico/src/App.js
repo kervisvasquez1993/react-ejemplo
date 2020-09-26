b12e80add5a877b6cc3142cf60c4a9fe3b38a1b4
kervisvasquez1993 <kervisvasquez22@gmail.com>
2020-09-25 21:26:53 -0400

    agregamos mdificacion del carpeta basico desde canaima

--- basico/src/App.js
+++ basico/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Header from './components/Header'
+import Footer from './components/Footer.js'
 
 function App() {
   return (
-    <div className="App">
-        <Header />
+    <div>
+        <Header/>
+        <Footer/>
     </div>
   );
 }
 
 export default App;
+//para no generar etiquetas html de mas agregamos Fra
---
+++ basico/src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+const Footer = () =>{
+    return(
+        <footer>
+            <p>Todos los derechos reservado &copy</p>
+        </footer>
+    )
+}
+export default Footer
