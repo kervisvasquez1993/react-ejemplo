8190e1d6226d07485bb50aec151f4276ac34bf42
kervisvasquez1993 <kervisvasquez22@gmail.com>
2020-09-28 07:08:46 -0400

    slucionamos prblema de compnente cita

--- citas/src/App.js
+++ citas/src/App.js
@@ -1,10 +1,26 @@
-import React, {Fragment, useState}  from 'react';
+import React, {Fragment, useState, useEffect}  from 'react';
 import Formulario from './components/Formulario';
 import Cita  from './components/Cita';
 function App() {
 
   // arreglo de citas
   const [citas, guardarCitas] = useState([])
+
+  // citas en localstorage
+  let citasIniciales = JSON.parse(localStorage.getItem('citas'))
+
+  if(!citasIniciales)
+  {
+    citasIniciales = []
+  }
+
+  // use effect para  realizar ciertas operaciones cuando el state cambie
+
+  useEffect( ()=> {
+   
+
+   
+  }, [citas] )
 
   // funcion que tome las citas actuales y agregue las nuevas 
 
@@ -17,6 +33,10 @@ function App() {
     const nuevasCitas = citas.filter(cita => cita.id !== id) // crea un nuevo arreglo
     guardarCitas(nuevasCitas)
   }
+
+  // mensaje condicional 
+
+  let titulo = citas.length === 0 ? 'No hay citas' : 'Administra tus Citas' 
   return (
   <Fragment>
       <h1>Formulario de contacto desde casa</h1>
@@ -28,7 +48,7 @@ function App() {
                 />
               </div>
               <div className="one-half column">
-                 <h2>Administra tus citas </h2>
+                 <h2>{titulo} </h2>
                  {citas.map( cita => (
                    <Cita 
                       key =  {cita.id}
